9a8215cc08fe32c1f37d68f0c6a8e60c69f17688
StutiSri <stutisrivastv@gmail.com>
2017-05-24 22:11:26 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int num = Integer.parseInt(args[0]);
+        for(int i=0;i<num;i++){
+            System.out.println("*"); }
+    }
+}
